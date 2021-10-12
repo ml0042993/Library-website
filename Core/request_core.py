e1e37cbf75f78fd69961d907b86a3f37bd2745b6
ml0042993 <zhoujianxiong.1988@gmail.com>
2021-10-12 23:43:59 +0800

    修改requests的获取方式，转为会话保持，调整参数为重试3次间隔0.5秒，以此解决MaxRetryError的问题

--- Core/request_core.py
+++ Core/request_core.py
@@ -1,17 +1,23 @@
 import requests
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 from pyquery import PyQuery as pq
 from Config.Configuration import Parameter
 # 得到输入网页的源代码
 class Requert_init:
 	def Get_requers(self,url,proxies=None):
 		try:
-			process_html=requests.get(url,headers=Parameter.HEARD.value,proxies=proxies,timeout=20)
-
+			# process_html=requests.get(url,headers=Parameter.HEARD.value,proxies=proxies,timeout=20)
+			session = requests.Session()
+			retry = Retry(connect=3, backoff_factor=0.5)
+			adapter = HTTPAdapter(max_retries=retry)
+			session.mount('http://', adapter)
+			session.mount('https://', adapter)
 		# encoding是从http中的header中的charset字段中提取的编码方式，若header中没有charset字段则默认为ISO-8859-1编码模式，则无法解析中文
 			# ，这是乱码的原因。
 		# apparent_encoding会从网页的内容中分析网页编码的方式，所以apparent_encoding比encoding更加准确。当网页出现乱码时可以把
 			# apparent_encoding的编码格式赋值给encoding。
-
+			process_html = session.get(url,headers=Parameter.HEARD.value,proxies=proxies,verify=False)
 			process_html.encoding = process_html.apparent_encoding#解决乱码问题
 			return process_html#返回首页的html源代码
 		except(ConnectionError):

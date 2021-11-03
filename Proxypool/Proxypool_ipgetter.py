268ac8c1f4f77ebd92d99b388abf8894a7e2ae7e
ml0042993 <zhoujianxiong.1988@gmail.com>
2021-11-04 01:29:30 +0800

    更新代理池

--- Proxypool/Proxypool_ipgetter.py
+++ Proxypool/Proxypool_ipgetter.py
@@ -20,55 +20,55 @@ class Proxy_Getter(object,metaclass=ProxyMetaclass):
 			proxies.append(proxy)
 		return proxies
 
-	def __ip_Get_jiangxianli(self,pages=4):
-		'''
-		:return:
-		'''
-		# proxy = {}#字典不能放到循环外部，会导致在入库时出现id重复的错误，pymongo.errors.DuplicateKeyError:
-		home_url = "https://ip.jiangxianli.com/?page={}&anonymity=2"
-		urls = [home_url.format(page) for page in range(1,pages+1)]
-		for url in urls:
-			print("开始获取" + url)
-			html = self.gp.Get_html(url)
-			if html:
-				trs = html('.layui-table tbody tr').items()
-				for tr in trs:
-					proxy = {}
-					proxy['Ip'] = tr.find("td:nth-child(1)").text()
-					proxy['Port'] = tr.find("td:nth-child(2)").text()
-					proxy['Type'] = tr.find("td:nth-child(3)").text()
-					proxy['Link'] = tr.find("td:nth-child(4)").text()
-					if len(proxy['Ip']) >= 16:
-						continue
-					yield proxy
+	# def __ip_Get_jiangxianli(self,pages=4):
+	# 	'''
+	# 	:return:
+	# 	'''
+	# 	# proxy = {}#字典不能放到循环外部，会导致在入库时出现id重复的错误，pymongo.errors.DuplicateKeyError:
+	# 	home_url = "https://ip.jiangxianli.com/?page={}&anonymity=2"
+	# 	urls = [home_url.format(page) for page in range(1,pages+1)]
+	# 	for url in urls:
+	# 		print("开始获取" + url)
+	# 		html = self.gp.Get_html(url)
+	# 		if html:
+	# 			trs = html('.layui-table tbody tr').items()
+	# 			for tr in trs:
+	# 				proxy = {}
+	# 				proxy['Ip'] = tr.find("td:nth-child(1)").text()
+	# 				proxy['Port'] = tr.find("td:nth-child(2)").text()
+	# 				proxy['Type'] = tr.find("td:nth-child(3)").text()
+	# 				proxy['Link'] = tr.find("td:nth-child(4)").text()
+	# 				if len(proxy['Ip']) >= 16:
+	# 					continue
+	# 				yield proxy
 		print('当前网址获取完成')
 
-	def __ip_Get_66ip(self,pages=10):
-		'''
-		:return:
-		'''
-		# proxy = {}#字典不能放到循环外部，会导致在入库时出现id重复的错误，pymongo.errors.DuplicateKeyError:
-		home_url = "http://www.66ip.cn/{}.html"
-		urls = [home_url.format(page) for page in range(1,pages+1)]
-		for url in urls:
-			print("开始获取" + url)
-			html = self.gp.Get_html(url)
-			# print(html)
-			if html:
-				trs = html('#main table tr').items()
-				for tr in trs:
-					proxy = {}
-					proxy['Ip'] = tr.find("td:nth-child(1)").text()
-					proxy['Port'] = tr.find("td:nth-child(2)").text()
-					proxy['Type'] = tr.find("td:nth-child(4)").text()
-					proxy['Link'] = "HTTP"
-					if len(proxy['Ip']) >= 16 or len(proxy['Ip'])<7:
-						continue
-					yield proxy
-		print('当前网址获取完成')
+	# def __ip_Get_66ip(self,pages=10):
+	# 	'''
+	# 	:return:
+	# 	'''
+	# 	# proxy = {}#字典不能放到循环外部，会导致在入库时出现id重复的错误，pymongo.errors.DuplicateKeyError:
+	# 	home_url = "http://www.66ip.cn/{}.html"
+	# 	urls = [home_url.format(page) for page in range(1,pages+1)]
+	# 	for url in urls:
+	# 		print("开始获取" + url)
+	# 		html = self.gp.Get_html(url)
+	# 		# print(html)
+	# 		if html:
+	# 			trs = html('#main table tr').items()
+	# 			for tr in trs:
+	# 				proxy = {}
+	# 				proxy['Ip'] = tr.find("td:nth-child(1)").text()
+	# 				proxy['Port'] = tr.find("td:nth-child(2)").text()
+	# 				proxy['Type'] = tr.find("td:nth-child(4)").text()
+	# 				proxy['Link'] = "HTTP"
+	# 				if len(proxy['Ip']) >= 16 or len(proxy['Ip'])<7:
+	# 					continue
+	# 				yield proxy
+	# 	print('当前网址获取完成')
 
 	def __ip_Get_kuaidaili(self, pages=10):
-		home_url = "https://www.kuaidaili.com/free/inha/{}/"
+		home_url = "https://www.kuaidaili.com/free/{}/"
 		urls = [home_url.format(page) for page in range(1,pages+1)]
 		for url in urls:
 			print("开始获取" + url)
@@ -88,7 +88,7 @@ class Proxy_Getter(object,metaclass=ProxyMetaclass):
 		print('当前网址获取完成')
 	def __ip_Get_ihuan(self, pages=None):
 		home_url = "https://ip.ihuan.me/?page={}"
-		list_page = ['b97827cc','4ce63706','5crfe930','f3k1d581','ce1d45977','881aaf7b5','eas7a436','981o917f5','2d28bd81a','a42g5985d','came0299']
+		list_page = ['b97827cc','4ce63706','5crfe930','f3k1d581','ce1d45977',]
 		urls = [home_url.format(page) for page in list_page]
 		for url in urls:
 			print("开始获取" + url)
@@ -101,6 +101,44 @@ class Proxy_Getter(object,metaclass=ProxyMetaclass):
 					proxy['Ip'] = tr.find("td:nth-child(1)").text()
 					proxy['Port'] = tr.find("td:nth-child(2)").text()
 					proxy['Type'] = tr.find("td:nth-child(7)").text()
+					proxy['Link'] = "HTTP"
+					if len(proxy['Ip']) >= 16 or len(proxy['Ip'])<7:
+						continue
+					yield proxy
+		print('当前网址获取完成')
+	def __ip_Get_7yip(self, pages=10):
+		home_url = "https://proxy.ip3366.net/free/?action=china&page={}"
+		urls = [home_url.format(page) for page in range(1, pages + 1)]
+		for url in urls:
+			print("开始获取" + url)
+			html = self.gp.Get_html(url)
+			# print(html)
+			if html:
+				trs = html('.container tbody tr').items()
+				for tr in trs:
+					proxy = {}
+					proxy['Ip'] = tr.find("td:nth-child(1)").text()
+					proxy['Port'] = tr.find("td:nth-child(2)").text()
+					proxy['Type'] = tr.find("td:nth-child(4)").text()
+					proxy['Link'] = "HTTP"
+					if len(proxy['Ip']) >= 16 or len(proxy['Ip'])<7:
+						continue
+					yield proxy
+		print('当前网址获取完成')
+	def __ip_Get_ip3366(self, pages=7):
+		home_url = "http://www.ip3366.net/free/?stype=3&page={}"
+		urls = [home_url.format(page) for page in range(1, pages + 1)]
+		for url in urls:
+			print("开始获取" + url)
+			html = self.gp.Get_html(url)
+			# print(html)
+			if html:
+				trs = html('#list tbody tr').items()
+				for tr in trs:
+					proxy = {}
+					proxy['Ip'] = tr.find("td:nth-child(1)").text()
+					proxy['Port'] = tr.find("td:nth-child(2)").text()
+					proxy['Type'] = tr.find("td:nth-child(3)").text()
 					proxy['Link'] = "HTTP"
 					if len(proxy['Ip']) >= 16 or len(proxy['Ip'])<7:
 						continue
